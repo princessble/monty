7d244fdc3ecc9b9f150a0bad7eac4ec14f86a47c
princessble <kwomofortuneb@gmail,com>
2023-06-21 09:40:42 -0700

    dv.c

--- dv.c
+++ dv.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * f_dv - divides the top two elements of the stack.
+ * f_div - divides the top two elements of the stack.
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_dv(stack_t **head, unsigned int counter)
+void f_div(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 	int len = 0, aux;
@@ -18,7 +18,7 @@ void f_dv(stack_t **head, unsigned int counter)
 	}
 	if (len < 2)
 	{
-		fprintf(stderr, "L%d: can't dv, stack too short\n", counter);
+		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
