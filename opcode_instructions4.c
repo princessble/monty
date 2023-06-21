6c0fb36638b8a10cf3e9a5030ed70d44d2e40160
Blessing Fortune <74838811+princessble@users.noreply.github.com>
2023-06-21 23:54:20 +0100

    Update opcode_instructions4.c

--- opcode_instructions4.c
+++ opcode_instructions4.c
@@ -2,7 +2,6 @@
 
 /**
  * _rotl - rotates the first element to the bottom and  the second to the top
- *
  * @doubly: head of the linked list
  * @cline: line number;
  * Return: no return
@@ -34,7 +33,6 @@ void _rotl(stack_t **doubly, unsigned int cline)
 
 /**
  * _rotr - reverse the stack
- *
  * @doubly: head of the linked list
  * @cline: line number
  * Return: no return
